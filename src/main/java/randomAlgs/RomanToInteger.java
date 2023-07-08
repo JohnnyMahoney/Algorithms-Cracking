ab32c9eab6e638e4c2c013434259fe289e0d5830
JohnnyMahoney <khomyk.dima42@gmail.com>
2023-07-08 22:08:44 +0200

    RomanToInteger algo

---
+++ src/main/java/randomAlgs/RomanToInteger.java
@@ -0,0 +1,32 @@
+package randomAlgs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+        String s = "MCMXCIV";
+        System.out.println(romanToInt(s));
+    }
+
+    public static int romanToInt(String s) {
+        Map<Character, Integer> m = new HashMap<>();
+        m.put('I', 1);
+        m.put('V', 5);
+        m.put('X', 10);
+        m.put('L', 50);
+        m.put('C', 100);
+        m.put('D', 500);
+        m.put('M', 1000);
+
+        int answer = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.length() - 1 != i && m.get(s.charAt(i)) < m.get(s.charAt(i + 1)))
+                answer = answer - m.get(s.charAt(i));
+            else
+                answer += m.get(s.charAt(i));
+        }
+        return answer;
+    }
+}
