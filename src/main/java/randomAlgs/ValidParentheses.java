6a1ba1985c2325219b0f567e36ea5ae24bb302f4
JohnnyMahoney <khomyk.dima42@gmail.com>
2023-07-21 22:54:28 +0200

    Valid Parentheses algo

--- src/main/java/randomAlgs/ValidParentheses.java
+++ src/main/java/randomAlgs/ValidParentheses.java
@@ -6,7 +6,6 @@ public class ValidParentheses {
 
     public static boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-
         for (char c : s.toCharArray()) {
             if (c == '(' || c == '[' || c == '{') {
                 stack.push(c);
@@ -25,7 +24,6 @@ public class ValidParentheses {
         }
         return stack.isEmpty();
     }
-
     public static void main(String[] args) {
         String s1 = "()";
         System.out.println(s1 + " is valid: " + isValid(s1));
