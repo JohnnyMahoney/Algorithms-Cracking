0c2b2cb27f7b406b6daf922b3bca994cfcd06bb8
dmytro <khomyk.dima42@gmail.com>
2024-08-09 19:04:48 -0700

    Remove the Node from the End - two pointer technique

---
+++ src/main/java/randomAlgs/RemoveNthLastNode.java
@@ -0,0 +1,51 @@
+package randomAlgs;
+
+public class RemoveNthLastNode {
+    public static LinkedListNode removeNthLastNode(LinkedListNode head, int n) {
+
+        LinkedListNode dummy = new LinkedListNode(0);
+        dummy.next = head;
+
+        LinkedListNode first = dummy;
+        LinkedListNode second = dummy;
+
+        for (int i = 0; i <= n; i++) {
+            first = first.next;
+        }
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        LinkedListNode linkedListNode1 = new LinkedListNode(1);
+        LinkedListNode linkedListNode2 = new LinkedListNode(2);
+        LinkedListNode linkedListNode3 = new LinkedListNode(3);
+        LinkedListNode linkedListNode4 = new LinkedListNode(4);
+        LinkedListNode linkedListNode5 = new LinkedListNode(5);
+        linkedListNode1.next = linkedListNode2;
+        linkedListNode2.next = linkedListNode3;
+        linkedListNode3.next = linkedListNode4;
+        linkedListNode4.next = linkedListNode5;
+        LinkedListNode modifiedHead = removeNthLastNode(linkedListNode1, 2);
+        LinkedListNode current = modifiedHead;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("NULL");
+    }
+    }
+
+class LinkedListNode {
+    public int data;
+    public LinkedListNode next;
+
+    public LinkedListNode(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
