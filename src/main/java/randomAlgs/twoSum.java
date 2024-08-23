3d48ba28a49e52ddc6b1914de8e47ab1287b40aa
dmytro <khomyk.dima42@gmail.com>
2024-08-22 18:30:27 -0700

    Two sum with two pointer technique

---
+++ src/main/java/randomAlgs/twoSum.java
@@ -0,0 +1,34 @@
+package randomAlgs;
+
+public class twoSum {
+
+    //Using the two-pointer tecnique
+    // if   sum of two == - return, else if sum < target - start++, else sum > target - end--
+    public static int[] twoSum(int[] arr, int target) {
+        int[] result = new int[2];
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            if ((arr[start] + arr[end]) == target) {
+                result[0] = start;
+                result[1] = end;
+                return result;
+            } else if ((arr[start] + arr[end]) < target) {
+                start++;
+            } else
+                end--;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        int[] result = twoSum(array, 8);
+        for (int num : result) {
+            System.out.println(num);
+        }
+    }
+}
