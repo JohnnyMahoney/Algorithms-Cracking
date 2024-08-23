d93f511a8d70ebed8d148939416c599b82ba5816
dmytro <khomyk.dima42@gmail.com>
2024-08-22 20:28:12 -0700

    Find missing positive extra space

--- src/main/java/randomAlgs/FirstMissingPositiveBrood.java
+++ src/main/java/randomAlgs/FirstMissingPositiveBrood.java
@@ -6,22 +6,22 @@ import java.util.Set;
 public class FirstMissingPositiveBrood {
 
     public static int findValue(int[] arr) {
-        int value  =0;
+        int value = 0;
         Set<Integer> uniqueValues = new HashSet<>();
 
-        for(int i =0; i< arr.length;i++) {
+        for (int i = 0; i < arr.length; i++) {
             uniqueValues.add(arr[i]);
         }
-        for(int i =0; i<= arr.length;i++) {
-            if(!uniqueValues.contains(i)) return i;
+        for (int i = 0; i <= arr.length; i++) {
+            if (!uniqueValues.contains(i)) return i;
         }
         return value;
     }
 
 
     public static void main(String[] args) {
-        int[] arr = {7,1,2,4,6,9,0,8};
-       int result = findValue(arr);
+        int[] arr = {7, 1, 2, 4, 6, 9, 0, 8};
+        int result = findValue(arr);
         System.out.println(result);
 
     }
