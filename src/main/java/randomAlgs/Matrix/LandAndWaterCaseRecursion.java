67c49dff08724defc0907e6cd81bd825f3dd0e16
dmytro <khomyk.dima42@gmail.com>
2024-09-02 20:58:46 -0700

    Island Matrix perimeter using iterative way

--- src/main/java/randomAlgs/Matrix/LandAndWaterCaseRecursion.java
+++ src/main/java/randomAlgs/Matrix/LandAndWaterCaseRecursion.java
@@ -1,9 +1,8 @@
-package randomAlgs;
+package randomAlgs.Matrix;
 
-public class LandAndWaterCase {
+public class LandAndWaterCaseRecursion {
 
     public static int numsIsIsland(char[][] grid) {
-
         int islandCount = 0;
 
         for(int i=0; i< grid.length;i++){
@@ -17,7 +16,7 @@ public class LandAndWaterCase {
         return islandCount;
     }
     public static void changeLandToWater(char[][]grid, int i, int j){
-        if(i<0 || i >= grid.length || j< 0 || j >= grid[0].length || grid[i][j] =='0') return;
+        if (i<0 || i >= grid.length || j< 0 || j >= grid[0].length || grid[i][j] =='0') return;
 
         grid[i][j] = '0';
         changeLandToWater(grid, i-1, j);
@@ -25,10 +24,7 @@ public class LandAndWaterCase {
         changeLandToWater(grid, i, j-1);
         changeLandToWater(grid, i, j+1);
 
-
     }
-
-
     public static void main(String[] args) {
         char[][] arr = {
                 {'0', '0', '0', '0'},
