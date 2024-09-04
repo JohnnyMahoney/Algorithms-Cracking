3acf5eb171fdae4eae9628aaae06a309313ce390
dmytro <khomyk.dima42@gmail.com>
2024-09-04 09:46:07 -0700

    MergeIntervals the best approach

--- src/main/java/randomAlgs/MergeIntervals/MergeIntervals.java
+++ src/main/java/randomAlgs/MergeIntervals/MergeIntervals.java
@@ -4,25 +4,28 @@ import java.util.Arrays;
 
 public class MergeIntervals {
 
+    /*
+        The array should be sorted for operation, otherwise use Array.sort - nLogn operations
+        Space complexity - O(1)
+        Time Complexity - O(n) - iterate over all array
+     */
     public static int[][] mergeIntervals(int[][] intervals) {
         int index = 0;
 
-       for (int i = 1; i < intervals.length; i++) {
-           if(intervals[index][1] >= intervals[i][0]){
-               intervals[index][1] = Math.max(intervals[index][1],intervals[i][1]);
-           }
-           else {
-               index++;
-               intervals[index]  = intervals[i];
-           }
-       }
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[index][1] >= intervals[i][0]) {
+                intervals[index][1] = Math.max(intervals[index][1], intervals[i][1]);
+            } else {
+                index++;
+                intervals[index] = intervals[i];
+            }
+        }
 
-        return Arrays.copyOf(intervals, index+1);
+        return Arrays.copyOf(intervals, index + 1);
     }
 
-
     public static void main(String[] args) {
-        int[][] intervals = {{2,4},{3,5},{4,5},{6,10},{12,14}};
+        int[][] intervals = {{2, 4}, {3, 5}, {4, 5}, {6, 10}, {12, 14}};
 
         int[][] mergedIntervals = mergeIntervals(intervals);
 
