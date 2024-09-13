42a9d0e99994568b7beb294acf296378880312ad
dmytro <khomyk.dima42@gmail.com>
2024-09-12 21:41:24 -0700

    Find first bad version using binary search - first approach

--- src/main/java/randomAlgs/ModifiedBinarySearch/FirstBadVersion.java
+++ src/main/java/randomAlgs/ModifiedBinarySearch/FirstBadVersion.java
@@ -5,17 +5,15 @@ public class FirstBadVersion {
     public static int firstBad(int[] versions, int n) {
 
         int start = 0;
-        int end = versions.length -1;
+        int end = versions.length - 1;
 
-        while(start <= end) {
-            int middle = (start+end) /2;
-            if(versions[middle] == n && versions[middle-1] != n) {
+        while (start <= end) {
+            int middle = (start + end) / 2;
+            if (versions[middle] == n && versions[middle - 1] != n) {
                 return middle;
-            }
-            else if(versions[middle-1] == n){
+            } else if (versions[middle - 1] == n) {
                 end = middle - 1;
-            }
-            else
+            } else
                 start = middle + 1;
 
         }
@@ -23,7 +21,7 @@ public class FirstBadVersion {
     }
 
     public static void main(String[] args) {
-        int[] versions = {1,2,3,4,4};
+        int[] versions = {1, 2, 3, 4, 4};
         int result = firstBad(versions, 4);
         System.out.println(result);
     }
