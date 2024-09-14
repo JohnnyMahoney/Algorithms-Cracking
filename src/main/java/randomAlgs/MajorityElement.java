c9967b6c9152835ef0abf8eda481c962ae694103
dmytro <khomyk.dima42@gmail.com>
2024-09-14 13:48:07 -0700

    Majority Element The best approach Time - O(n), Space - O(1)

--- src/main/java/randomAlgs/MajorityElement.java
+++ src/main/java/randomAlgs/MajorityElement.java
@@ -34,7 +34,7 @@ public class MajorityElement {
     }
 
     public static void main(String[] args) {
-        int[] arr = {2, 2, 1, 1, 2, 2};
+        int[] arr = {1,2,1,3,1,4,1,5,1,6,1,7,1,8,1,9};
         int result1 = majorityElementFirst(arr);
         int result2 = majorityElementSecond(arr);
         System.out.println(result1);
