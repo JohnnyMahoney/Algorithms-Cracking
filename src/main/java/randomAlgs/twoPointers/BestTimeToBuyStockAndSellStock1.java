56164dd7610f09ce3a8672f56ae3c9320c4eb7d9
dmytro <khomyk.dima42@gmail.com>
2024-09-13 22:49:38 -0700

    BestTimeToBuyStockAndSellStock1 slow solution - O(n^2) two loops, one nested

--- src/main/java/randomAlgs/twoPointers/BestTimeToBuyStockAndSellStock1.java
+++ src/main/java/randomAlgs/twoPointers/BestTimeToBuyStockAndSellStock1.java
@@ -5,11 +5,11 @@ public class BestTimeToBuyStockAndSellStock1 {
     public static int maxProfit(int[] prices) {
 
         int profit = 0;
-        for (int i = 0; i < prices.length ; i++) {
+        for (int i = 0; i < prices.length; i++) {
             int start = i;
-            int end = prices.length -1;
-            while(start < end) {
-                if((prices[end] - prices[start]) > profit) {
+            int end = prices.length - 1;
+            while (start < end) {
+                if ((prices[end] - prices[start]) > profit) {
                     profit = prices[end] - prices[start];
                 }
                 end--;
@@ -20,7 +20,7 @@ public class BestTimeToBuyStockAndSellStock1 {
 
 
     public static void main(String[] args) {
-        int [] prices = {7,1,5,3,6,4};
+        int[] prices = {7, 1, 5, 3, 6, 4};
 
         int result = maxProfit(prices);
         System.out.println(result);
