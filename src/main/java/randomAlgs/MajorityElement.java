08a92b4196511c1a71530e9f66beda7c802c9e73
dmytro <khomyk.dima42@gmail.com>
2024-09-14 13:37:21 -0700

    Majority Element The best approach Time - O(n), Space - O(1)

--- src/main/java/randomAlgs/MajorityElement.java
+++ src/main/java/randomAlgs/MajorityElement.java
@@ -16,13 +16,21 @@ public class MajorityElement {
                 return num;
             }
         }
-
         return -1;
     }
 
     public static int majorityElementSecond(int[] nums) {
+        int candidate = 0;
+        int count = 0;
 
-        return -1;
+        for(int num : nums) {
+            if(count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 :-1;
+        }
+
+        return candidate;
     }
 
     public static void main(String[] args) {
