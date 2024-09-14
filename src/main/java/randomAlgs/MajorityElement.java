309bb2ffcd39a3029656936e242afd5ab7cdda4f
dmytro <khomyk.dima42@gmail.com>
2024-09-14 13:16:48 -0700

    Majority Element Naive approach

---
+++ src/main/java/randomAlgs/MajorityElement.java
@@ -0,0 +1,35 @@
+package randomAlgs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement {
+
+    public static int majorityElementFirst(int[] nums) {
+        Map<Integer,Integer> map  = new HashMap<>();
+        int majorityCount = nums.length /2;
+
+        for(int num: nums) {
+            map.put(num, map.getOrDefault(num,0) +1);
+
+            if(map.get(num) > majorityCount) {
+                return num;
+            }
+        }
+
+        return -1;
+    }
+
+    public static int majorityElementSecond(int[] nums) {
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,2,1,1,2,2};
+        int result1 = majorityElementFirst(arr);
+        int result2 = majorityElementSecond(arr);
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+}
