dc417ea00bd3eb6a13f49edb6de71345db0db661
dmytro <khomyk.dima42@gmail.com>
2024-09-17 14:57:07 -0700

    structure cleaning

--- src/main/java/randomAlgs/GreedyTechnique/JumpGameGreedyAlgorithm.java
+++ src/main/java/randomAlgs/GreedyTechnique/JumpGameGreedyAlgorithm.java
@@ -5,23 +5,23 @@ public class JumpGameGreedyAlgorithm {
         int n = nums.length;
         int maxReach = 0;
 
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
 
-            if(i > maxReach) {
+            if (i > maxReach) {
                 return false;
             }
             maxReach = Math.max(maxReach, i + nums[i]);
 
-            if( maxReach >= n -1) return true;
+            if (maxReach >= n - 1) return true;
         }
 
         return false;
     }
 
     public static void main(String[] args) {
-        int[] nums1 = {2,3,1,1,4};
+        int[] nums1 = {2, 3, 1, 1, 4};
         System.out.println("Can jump to the end: " + canJump(nums1)); // Output: true
-        int[] nums2 = {3,2,1,0,4};
+        int[] nums2 = {3, 2, 1, 0, 4};
         System.out.println("Can jump to the end: " + canJump(nums2)); // Output: false
     }
 }
