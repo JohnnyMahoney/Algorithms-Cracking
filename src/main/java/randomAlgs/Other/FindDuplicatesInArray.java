ee804b6102ece70e034653228495f6459af4f49e
dmytro <khomyk.dima42@gmail.com>
2024-09-17 11:40:03 -0700

    structure cleaning

--- src/main/java/randomAlgs/Other/FindDuplicatesInArray.java
+++ src/main/java/randomAlgs/Other/FindDuplicatesInArray.java
@@ -8,11 +8,11 @@ public class FindDuplicatesInArray {
     public static List<Integer> duplicates(int[] arr) {
         List<Integer> duplicates = new ArrayList<>();
 
-        for(int i = 0; i < arr.length; i++) {
-            int element = Math.abs(arr[i]) -1;
-            if(arr[element] < 0 ) duplicates.add(Math.abs(arr[i]));
+        for (int i = 0; i < arr.length; i++) {
+            int element = Math.abs(arr[i]) - 1;
+            if (arr[element] < 0) duplicates.add(Math.abs(arr[i]));
 
-            arr[element] = - arr[element];
+            arr[element] = -arr[element];
         }
 
 
@@ -20,11 +20,10 @@ public class FindDuplicatesInArray {
     }
 
 
-
     public static void main(String[] args) {
-        int[] arr = {5,8,2,3,7,8,5,2};
-       List<Integer> duplicates =  duplicates(arr);
-       String answer =  duplicates.toString();
+        int[] arr = {5, 8, 2, 3, 7, 8, 5, 2};
+        List<Integer> duplicates = duplicates(arr);
+        String answer = duplicates.toString();
         System.out.println(answer);
 
     }
