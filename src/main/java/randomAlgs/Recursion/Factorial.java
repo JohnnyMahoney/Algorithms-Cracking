208fb308c1ef68da2de305edb845e9d5df68d03c
dmytro <khomyk.dima42@gmail.com>
2024-09-24 13:49:25 -0700

    Simple recursion problem

--- src/main/java/randomAlgs/Recursion/Factorial.java
+++ src/main/java/randomAlgs/Recursion/Factorial.java
@@ -1,6 +1,8 @@
 package randomAlgs.Recursion;
 
 public class Factorial {
+    //call stack - every recursion operation added in so-called call stack.
+    // And beginning from n=2 each operation will execute
     public static int factorial(int n) {
 
         if (n == 1)
@@ -8,7 +10,6 @@ public class Factorial {
         else
             return n * factorial(n - 1);
     }
-
 
     public static void main(String[] args) {
         int n = factorial(5);
