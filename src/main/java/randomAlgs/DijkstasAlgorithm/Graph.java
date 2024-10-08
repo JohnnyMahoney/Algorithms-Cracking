ebd8c7bd58e9bb30a658d224befe4ae975e8e4fd
dmytro <khomyk.dima42@gmail.com>
2024-10-07 17:18:18 -0700

    Dijkstra Shortest Path Algorithm implementation

---
+++ src/main/java/randomAlgs/DijkstasAlgorithm/Graph.java
@@ -0,0 +1,19 @@
+package randomAlgs.DijkstasAlgorithm;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+public class Graph {
+
+    private Set<Node> nodes = new HashSet<>();
+
+    public void addNode(Node nodeA) {
+        nodes.add(nodeA);
+    }
+
+}
