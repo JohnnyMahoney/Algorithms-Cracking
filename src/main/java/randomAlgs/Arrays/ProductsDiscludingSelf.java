f032bf9f246a5217a1adbecff5bbc63a7a0ae74a
dmytro <khomyk.dima42@gmail.com>
2024-10-18 14:26:53 -0700

    Product Discluding Array The best approach

--- src/main/java/randomAlgs/Arrays/ProductsDiscludingSelf.java
+++ src/main/java/randomAlgs/Arrays/ProductsDiscludingSelf.java
@@ -30,16 +30,13 @@ public class ProductsDiscludingSelf {
         for (int i = 0; i < n; i++) {
             result[i] = prefix;
             prefix = prefix * nums[i];
-
         }
 
         int postfix = 1;
-        for (int i = n-1; i >= 0; i--) {
+        for (int i = n - 1; i >= 0; i--) {
             result[i] = postfix * result[i];
             postfix = postfix * nums[i];
-
         }
-
         return result;
     }
 
