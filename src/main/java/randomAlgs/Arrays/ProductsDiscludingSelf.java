35031a6aed2fafc18098eb46b72ee9b9545bef1c
dmytro <khomyk.dima42@gmail.com>
2024-10-18 14:25:43 -0700

    Product Discluding Array The best approach

--- src/main/java/randomAlgs/Arrays/ProductsDiscludingSelf.java
+++ src/main/java/randomAlgs/Arrays/ProductsDiscludingSelf.java
@@ -2,6 +2,7 @@ package randomAlgs.Arrays;
 
 public class ProductsDiscludingSelf {
 
+    //Time Complexity - O(n^2)
     public static int[] productExceptSelf(int[] nums) {
         int[] result = new int[nums.length];
 
@@ -20,9 +21,30 @@ public class ProductsDiscludingSelf {
         return result;
     }
 
+    public static int[] productExceptSelf2(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        int prefix = 1;
+        for (int i = 0; i < n; i++) {
+            result[i] = prefix;
+            prefix = prefix * nums[i];
+
+        }
+
+        int postfix = 1;
+        for (int i = n-1; i >= 0; i--) {
+            result[i] = postfix * result[i];
+            postfix = postfix * nums[i];
+
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
-        int[] arr = {1, 2, 4, 6};
-        int[] result = productExceptSelf(arr);
+        int[] arr = {1, 2, 3, 4};
+        int[] result = productExceptSelf2(arr);
         for (int i : result) {
             System.out.println(i + " ");
         }
