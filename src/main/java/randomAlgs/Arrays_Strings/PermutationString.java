6f824c2c80bfc0d2e735f1dfda21a3b26aa8679e
dmytro <khomyk.dima42@gmail.com>
2024-11-12 09:19:24 -0800

    Sliding Window Permutation String

---
+++ src/main/java/randomAlgs/Arrays_Strings/PermutationString.java
@@ -0,0 +1,13 @@
+package randomAlgs.Arrays_Strings;
+
+public class PermutationString {
+    public static boolean checkInclusion(String s1, String s2) {
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+       boolean result =  checkInclusion("abc","lecabee");
+        System.out.println(result);
+    }
+}
