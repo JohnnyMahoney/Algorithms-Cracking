c8ec4d65196950c5bbff3b3e996d4db3cac8c6fb
dmytro <khomyk.dima42@gmail.com>
2024-11-26 22:11:46 -0800

    FindMinimumInRotatedSortedArray problem using binary search

---
+++ src/main/java/randomAlgs/BinarySearch/FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,37 @@
+package randomAlgs.BinarySearch;
+
+public class FindMinimumInRotatedSortedArray {
+    public static int findMin(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        if (nums[start] <= nums[end] || nums.length == 1) {
+            return nums[0];
+        }
+
+        if (nums.length == 2) {
+            return Math.min(nums[0], nums[1]);
+        } else {
+            while (start < end) {
+                int middle = start + (end - start) / 2;
+                if (middle > 0 && nums[middle] < nums[middle - 1]) {
+                    return nums[middle];
+                } else if (nums[middle] > nums[end]) {
+                    start = middle + 1;
+                } else {
+                    end = middle;
+                }
+
+            }
+        }
+        return nums[start];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,3,1};
+
+        int result = findMin(nums);
+        System.out.println(result);
+
+    }
+}
